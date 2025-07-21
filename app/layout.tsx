5f54417e776af394d65a5368cc47bb8247179c66
valentinsg <sanchezguevaravalentin@gmail.com>
2025-07-21 19:54:25 -0300

    pixel agregado

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/sonner"
 import type { Metadata } from "next"
 import { Inter, Source_Sans_3 } from "next/font/google"
+import Script from "next/script"
 import type React from "react"
 import "./globals.css"
 
@@ -28,6 +29,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning className={`${sourceSans.variable}`}>
+      <head>
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1481766756163592');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1481766756163592&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={`${inter.className} ${sourceSans.variable}`} suppressHydrationWarning>
         {children}
         <Toaster
